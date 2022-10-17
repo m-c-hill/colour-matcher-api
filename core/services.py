f5cda7356d8ef53c8864f8444e73f41b23cf0178
m-c-hill <matthewhill2595@gmail.com>
2022-10-18 00:37:21 +0100

    colour match functionality in services.py - match colour, compress image and calculate colour differences

--- core/services.py
+++ core/services.py
@@ -1,9 +1,16 @@
+import math
+from nis import match
 from typing import Tuple
+import requests
+import numpy as np
+from urllib.request import urlopen
 from PIL import Image
 
 from .schemas import Colour
 
 COMPRESSION_SIZE = 100
+
+# TODO: documnetation
 
 palette = [
     {"name": "salmon", "rgb": (250, 128, 114)},
@@ -13,30 +20,72 @@ palette = [
 
 
 def is_url_image(url: str) -> bool:
-    pass
+    image_formats = ("image/png", "image/jpeg", "image/jpg")
+    r = requests.head(url)
+    return r.headers["content-type"] in image_formats
 
 
 def match_colour(url: str) -> bool:
-    pass
+    pixel_cache = {}
+    colour_count = {}
+
+    image = Image.open(urlopen(url))
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+
+    image = compress_image(image)
+    image_arr = np.array(image)
+
+    min_diff = math.inf  # TODO: doesn't seem right... think about eqn
+    for row in image_arr:
+        for pixel in row:
+            rgb_pixel = tuple(pixel)
+
+            if rgb_pixel in pixel_cache:
+                colour_name = pixel_cache[rgb_pixel]["matched_colour"]
+                colour_count[colour_name] += 1
+                continue
+
+            for colour in palette:
+                rgb_palette = colour["rgb"]
+                diff = get_redmean_colour_difference(rgb_pixel, rgb_palette)
+                min_diff = min(min_diff, diff)
+                pixel_cache[rgb_pixel] = {
+                    "matched_colour": colour["name"],
+                    "diff": diff,
+                }
+                colour_count[colour["name"]] = 1
+
+    # TODO: add an upper bound to min diff to return null result if no colour matches correctly
+
+    matched_colour = max(colour_count, key=colour_count.get)
+    return {"url": url, "matched_colour": matched_colour, "diff": min_diff}
 
 
 def compress_image(image: Image) -> Image:
-    pass
+    if image.height <= COMPRESSION_SIZE:
+        return image
+
+    height = COMPRESSION_SIZE
+    width = int(COMPRESSION_SIZE / image.height * image.width)
+
+    return image.resize((height, width))
 
 
-# ===============================================
-
-
-def get_redmean_colour_difference(
-    colour1: Tuple[float], colour2: Tuple[float]
-) -> float:
+def get_redmean_colour_difference(colour1: Tuple[int], colour2: Tuple[int]) -> float:
     """
     Source: https://www.compuphase.com/cmetric.htm
     """
-    pass
+    r1, g1, b1 = colour1
+    r2, g2, b2 = colour2
 
+    redmean_factor = 0.5 * (r1 + r2)
+    d_red = r1 - r2
+    d_green = g1 - g2
+    d_blue = b1 - b2
 
-if __name__ == "__main__":
-    match_colour(
-        "https://pwintyimages.blob.core.windows.net/samples/stars/test-sample-teal.png"
+    return math.sqrt(
+        (2 + redmean_factor / 256) * math.pow(d_red, 2)
+        + 4 * math.pow(d_green, 2)
+        + (2 + (255 - redmean_factor) / 256) * math.pow(d_blue, 2)
     )
